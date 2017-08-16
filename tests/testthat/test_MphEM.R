dc085adf7058468d85708fd7773ada1c2541e242
Frederick Boehm <frederick.boehm@gmail.com>
2017-08-16 16:06:46 -0500

    started writing a MphEM test

---
+++ tests/testthat/test_MphEM.R
@@ -0,0 +1,25 @@
+library(gemma2)
+library(readr)
+read_csv("~/GEMMA/example/mouse100.geno.txt", col_names = FALSE) -> geno
+read_tsv("~/GEMMA/example/mouse100.pheno.txt", col_names = FALSE) -> pheno
+read_tsv("~/GEMMA/example/output/mouse100.cXX.txt", col_names = FALSE)[, 1:100] -> kinship
+e_out <- eigen2(kinship)
+library(tidyverse)
+# isolate first SNP
+g1 <- geno[1, - c(1:3)]
+g2m <- geno[2, - c(1:3)] %>%
+  as.matrix() %>%
+  t()
+t(as.matrix(g1)) -> g1m
+as.matrix(pheno[, c(1, 6)]) -> phe16
+
+context("Testing MphEM() with two phenotypes and no missing data - case of pleiotropy")
+
+test_that("First calculation of off-diagonal term for Vg is accurate", {
+  expect_equal(MphEM(max_iter = 10, eval = e_out$values, X = t(g1m) %*% e_out$vectors,
+               Y = t(phe16) %*% e_out$vectors, 
+               V_g = matrix(c(1.91352, 0, 0, 0.530827), nrow = 2),
+               V_e = matrix(c( 0.320028, 0, 0, 0.561589), nrow = 2)
+               )[[1]][[2]], 0.0700492)
+  
+})
