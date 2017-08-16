51dc136af260eb50b83ba6642aa092b554d3e155
Frederick Boehm <frederick.boehm@gmail.com>
2017-08-16 12:01:14 -0500

    fixed logl_new calcs

--- R/MphCalcLogL.R
+++ R/MphCalcLogL.R
@@ -163,12 +163,13 @@ MphEM2 <- function(max_iter = 10000, max_prec = 1 / 1000, eval, X1, X2, Y, V_g =
     #
     UltVehiY <- UltVehi %*% Y
     #
-    xHiy <- calc_XHiY(eval, D_l, X1, UltVehiY)
-    logl_new <- logl_const + MphCalcLogL(eval = eval, xHiy = xHiy,
+    xHiy1 <- calc_XHiY(eval, D_l, X1, UltVehiY)
+    xHiy2 <- calc_XHiY(eval, D_l, X2, UltVehiY)
+    logl_new <- logl_const + MphCalcLogL(eval = eval, xHiy = c(xHiy1, xHiy2),
                                          D_l = D_l, UltVehiY = UltVehiY,
                                          Qi = Qi) - 0.5 * n_size * logdet_Ve
     #if (func_name=='R') {
-    logl_new <- logl_new - 0.5 * (lndetQ1 + lndetQ2 - c_size * logdet_Ve) # do we need the last term to be 2 * c_size * logdet_Ve??
+    logl_new <- logl_new -  (lndetQ1 + lndetQ2 - c_size * logdet_Ve) # do we need the last term to be 2 * c_size * logdet_Ve??
     #}
     if (t > 1){
       if (logl_new - logl_old < max_prec){break}
@@ -178,7 +179,7 @@ MphEM2 <- function(max_iter = 10000, max_prec = 1 / 1000, eval, X1, X2, Y, V_g =
     co_out <- calc_omega(eval, D_l)
     co_out[[1]] -> OmegaU
     co_out[[2]] -> OmegaE
-    UltVehiB <- UpdateRL_B(xHiy, Qi, d_size = nrow(Y))
+    UltVehiB <- UpdateRL_B(c(xHiy1, xHiy2), Qi, d_size = nrow(Y))
     UltVehiBX <- UltVehiB %*% X
 
     UltVehiU <- update_u(OmegaE, UltVehiY, UltVehiBX)
