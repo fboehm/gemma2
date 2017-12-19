e1ed401e2bc29f3e3fab2e166bc9f1f2a5d98292
Frederick Boehm <frederick.boehm@gmail.com>
2017-12-19 08:56:48 -0600

    minor edits to documentation

--- R/MphCalcLogL.R
+++ R/MphCalcLogL.R
@@ -1,14 +1,14 @@
 #' Calculate log likelihood
 #'
 #' @param eval eigenvalues vector from decomposition of relatedness matrix
-#' @param D_l vector
+#' @param D_l vector of eigenvalues from decomposition of Ve matrix
 #' @param Qi inverse of Q matrix
 #' @param UltVehiY matrix of (transformed) Y values
 #' @param xHiy vector
 #' @export
 MphCalcLogL <- function(eval, D_l, Qi, UltVehiY, xHiy){
   n_size <- length(eval)
-  d_size <- length(D_l)
+  d_size <- length(D_l) # d is number of phenotypes
   dc_size <- nrow(Qi)
   logl <- 0
   for (k in 1:n_size){
@@ -28,13 +28,13 @@ MphCalcLogL <- function(eval, D_l, Qi, UltVehiY, xHiy){
 }
 
 
-#' Perform EM algorithm
+#' Perform EM algorithm for multivariate LMM
 #'
 #' @param max_iter maximum number of iterations for EM algorithm
 #' @param max_prec maximum precision for EM algorithm
 #' @param eval vector of eigenvalues from relatedness matrix decomposition
-#' @param X design matrix
-#' @param Y matrix of phenotype values
+#' @param X (rotated) design matrix
+#' @param Y (rotated) matrix of phenotype values
 #' @param V_g genetic covariance matrix
 #' @param V_e error covariance matrix
 #' @export
@@ -42,7 +42,6 @@ MphEM <- function(max_iter = 10000, max_prec = 1 / 1000,
                   eval, X, Y, V_g, V_e){
   n_size <- length(eval)
   # be careful with defining c_size here
-
   c_size <- nrow(X)
   d_size <- nrow(Y)
   dc_size <- c_size * d_size
@@ -75,7 +74,7 @@ MphEM <- function(max_iter = 10000, max_prec = 1 / 1000,
     logl_new <- logl_const + MphCalcLogL(eval = eval, xHiy = xHiy,
                                          D_l = D_l, UltVehiY = UltVehiY,
                                          Qi = Qi) - 0.5 * n_size * logdet_Ve
-    #if (func_name=='R') {
+    #if (func_name=='R') { # WE ALWAYS DO REML!!
     logl_new <- logl_new - 0.5 * (lndetQ - c_size * logdet_Ve)
     #}
     if (t > 1){
@@ -110,100 +109,6 @@ MphEM <- function(max_iter = 10000, max_prec = 1 / 1000,
   return(out)
 }
 
-#' Perform EM algorithm for pleiotropy or close linkage
-#'
-#' @param max_iter maximum number of iterations for EM algorithm
-#' @param max_prec maximum precision for EM algorithm
-#' @param eval vector of eigenvalues from relatedness matrix decomposition
-#' @param X1 transformed genotype probabilities matrix 1
-#' @param X2 transformed genotype probabilities matrix 2
-#' @param Y transformed matrix of phenotype values
-#' @param V_g genetic covariance matrix
-#' @param V_e error covariance matrix
-#' @export
-MphEM2 <- function(max_iter = 10000, max_prec = 1 / 1000, eval, X1, X2, Y, V_g = diag(1, 2), V_e = diag(1, 2)){
-  n_size <- length(eval)
-  # be careful with defining c_size here
-
-  c_size <- nrow(X1)
-  d_size <- nrow(Y)
-  dc_size <- c_size * d_size
-  # convert Y to vector Yv
-  Yv <- t(as.vector(t(Y)))
-  # stagger X1 & X2
-  X <- t(stagger_mats(t(X1), t(X2)))
-  # calculate XXt and XXti
-  XXt <- X %*% t(X)
-  #XXti <- solve(XXt)
-  ## XXti is only used for unrestricted likelihood!
-  # calculate constant for logl for REML
-  #if (func_name == "R"){
-  logl_const <- - (n_size - c_size) * d_size * log(2 * pi) + d_size * log(det(XXt)) / 2
-  #}
-  out <- list()
-  #logl_old <- 1 # we need to define logl_old before using it within the EM iterations
-  # start EM
-  for (t in 1:max_iter){
-    # eigen_proc
-    ep_out <- eigen_proc(V_g, V_e)
-    ep_out[[1]] -> logdet_Ve
-    ep_out[[2]] -> UltVeh
-    ep_out[[3]] -> UltVehi
-    ep_out[[4]] -> D_l
-    # calc_qi
-    cq_out <- calc_qi(eval, D_l, X1)
-    cq_out[[1]] -> Qi1
-    cq_out[[2]] -> lndetQ1
-    #
-    cq_out <- calc_qi(eval, D_l, X2)
-    cq_out[[1]] -> Qi2
-    cq_out[[2]] -> lndetQ2
-    #
-    Qi <- stagger_mats(Qi1, Qi2)
-    #
-    UltVehiY <- UltVehi %*% Y
-    #
-    xHiy1 <- calc_XHiY(eval, D_l, X1, UltVehiY)
-    xHiy2 <- calc_XHiY(eval, D_l, X2, UltVehiY)
-    logl_new <- logl_const + MphCalcLogL(eval = eval, xHiy = c(xHiy1, xHiy2),
-                                         D_l = D_l, UltVehiY = UltVehiY,
-                                         Qi = Qi) - n_size * logdet_Ve
-    #if (func_name=='R') {
-    logl_new <- logl_new -  (lndetQ1 + lndetQ2 - c_size * logdet_Ve)
-    #}
-    if (t > 1){
-      if (logl_new - logl_old < max_prec){break}
-    }
-    #if (t > 1 & abs(logl_new - logl_old) < max_prec) {break}
-    logl_old <- logl_new
-    co_out <- calc_omega(eval, D_l)
-    co_out[[1]] -> OmegaU
-    co_out[[2]] -> OmegaE
-    UltVehiB <- UpdateRL_B(c(xHiy1, xHiy2), Qi, d_size = nrow(Y))
-    #UltVehiBX <- UltVehiB %*% X
-    foo <- UltVehiB %*% X # This gives repeated entries.
-    UltVehiBX <- rbind(foo[1, 1:n_size], foo[2, (n_size + 1):(2 * n_size)])
-
-    UltVehiU <- update_u(OmegaE, UltVehiY, UltVehiBX)
-    UltVehiE <- update_e(UltVehiY, UltVehiBX, UltVehiU)
-    U_hat <- t(UltVeh) %*% UltVehiU
-    E_hat <- t(UltVeh) %*% UltVehiE
-    B <- t(UltVeh) %*% UltVehiB
-    cs_out <- calc_sigma(eval = eval, D_l = D_l,
-                         X = X, OmegaU = OmegaU, OmegaE = OmegaE,
-                         UltVeh = UltVeh, Qi = Qi)
-    cs_out[[1]] -> Sigma_ee
-    cs_out[[2]] -> Sigma_uu
-    uv_out <- update_v(eval, U_hat, E_hat, Sigma_uu, Sigma_ee)
-    # update V_g and V_e
-    uv_out[[1]] -> V_g
-    uv_out[[2]] -> V_e
-    out[[t]] <- list(logl_new, V_g, V_e, Sigma_uu, Sigma_ee, B,
-                     U_hat, E_hat, OmegaU, OmegaE, logdet_Ve, UltVeh, UltVehi,
-                     D_l, xHiy, logl_const, UltVehiU)
-  }
-  return(out)
-}
 
 #' Stagger two matrices of same size within a larger, block-diagonal matrix
 #'
