6f1774e9d345c2c2a62811be49821d534e56c846
Frederick Boehm <frederick.boehm@gmail.com>
2018-01-13 14:22:37 -0600

    added extra expect_equal statements for MphEM output.

--- tests/testthat/test_MphEM.R
+++ tests/testthat/test_MphEM.R
@@ -1,7 +1,4 @@
 library(gemma2)
-library(readr)
-#read_tsv("~/GEMMA/example/mouse100.pheno.txt", col_names = FALSE) -> pheno
-#read_tsv("~/GEMMA/example/output/mouse100.cXX.txt", col_names = FALSE)[, 1:100] -> kinship
 readr::read_tsv(system.file("extdata", "mouse100.pheno.txt", package = "gemma2"), col_names = FALSE) -> pheno
 readr::read_tsv(system.file("extdata", "mouse100.cXX.txt", package = "gemma2"), col_names = FALSE)[, 1:100] -> kinship
 
@@ -21,4 +18,6 @@ context("Testing MphEM() with two phenotypes and intercept only (no genetic data
 test_that("First calculations of for Vg and for Ve are accurate", {
   expect_equal(foo[[1]][[2]], matrix(c(1.91352, 0.0700492, 0.0700492, 0.530827), nrow = 2, ncol = 2, byrow = FALSE), tolerance = 0.000001)
   expect_equal(foo[[1]][[3]], matrix(c(0.320028, 0.0691272, 0.0691272, 0.561589), nrow = 2, ncol = 2, byrow = FALSE), tolerance = 0.000001)
+  expect_equal(foo[[5]][[2]], matrix(c(1.89405, 0.112242, 0.112242, 0.531075), nrow = 2), tolerance = 0.00001)
+  expect_equal(foo[[5]][[3]], matrix(c(0.324097, 0.153797, 0.153797, 0.561429), nrow = 2), tolerance = 0.00001)
 })
