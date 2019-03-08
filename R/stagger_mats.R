cad77d7ea2a09f5f49021ead8a89c9d903cced87
Frederick Boehm <frederick.boehm@gmail.com>
2019-03-08 13:31:26 -0600

    updated stagger_mats documentation

--- R/stagger_mats.R
+++ R/stagger_mats.R
@@ -1,7 +1,15 @@
 #' Stagger matrices within a larger, block-diagonal matrix
 #'
 #' @param ... one or more matrices, separated by commas
+#' @return a block-diagonal matrix, with the inputted matrices as blocks on the diagonal.
+#' @examples
+#' foo <- rnorm(40000, ncol = 8)
+#' block_diag <- stagger_mats(foo, foo)
+#' dim(foo)
+#' dim(block_diag)
 #' @export
+
+
 
 stagger_mats <- function(...){
   out <- as.matrix(Matrix::bdiag(...))
